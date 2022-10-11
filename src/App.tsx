23b5f6ce6ca1d86f3b1ac4b05bb25ce9c38f4cf5
aleksandr.smirnova1 <aleksandr.smirnova1@x5.ru>
2022-10-11 17:16:27 +0300

    VideoChat compt, savepoint EventEmitter on hooks, mediaDevice and peerConnection func compts

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
 import { useEffect, useState } from 'react';
 import { v4 as uuid } from 'uuid';
+import PeerConnection from './peerConnection';
 import VideoChat from './components/VideoChat';
 import TextChat from './components/TextChat';
+import myWs from './ws';
 import styles from './App.module.scss';
-
-const PORT = 9000;
-const URL = 'karpova123.site';
-const myWs = new WebSocket(`wss://${URL}:${PORT}`);
 
 function App() {
   const [clientId, setClientId] = useState<string>('');
+  const [config, setConfig] = useState<{ audio: boolean, video: boolean }>({ audio: false, video: false});
+  const [pc, setPC] = useState<RTCPeerConnection | null>(null);
 
   useEffect(() => {
     const id = uuid();
     setClientId(id);
   }, [])
+
+  function startCall({isCaller, friendId, config}: { 
+    isCaller: Boolean, 
+    friendId: Number, 
+    config: {
+      audio: boolean, 
+      video: boolean
+    } 
+  }) {
+    setConfig(config);
+    setPC(new PeerConnection({friendId}));
+      .on('localStream', (src) => {
+        const newState = { callWindow: 'active', localSrc: src };
+        if (!isCaller) newState.callModal = '';
+        this.setState(newState);
+      })
+      .on('peerStream', (src) => this.setState({ peerSrc: src }))
+      .start(isCaller);
+  }
 
   return (
     <div className={styles.App}>
@@ -22,7 +41,7 @@ function App() {
         <div className={styles.content}>
           {/* <span id="status">Connecting...</span> */}
           <div className={styles.videoChat}>
-            <VideoChat/>
+            <VideoChat startCall={startCall}/>
           </div>
 
           <div className={styles.textChat}>
---
+++ src/peerConnection.tsx
@@ -0,0 +1,24 @@
+import myWs from "./ws";
+import MediaDevice from "./mediaDevice";
+
+const PC_CONFIG = {};
+
+const PeerConnection = ({friendId}: {friendId: string}) => {
+  const pc = new RTCPeerConnection(PC_CONFIG);
+  pc.onicecandidate = (evt) => {
+    const { candidate } = evt;
+
+    myWs.send(JSON.stringify({ action: 'call', data: {
+      to: friendId,
+      candidate,
+    }}));
+  }
+
+  pc.ontrack = (evt) => {
+    myWs.send(JSON.stringify({ action: 'peerStream', data: evt.streams[0] }))
+  }
+
+  const mediaDevice = new MediaDevice();
+}
+
+export default PeerConnection;
